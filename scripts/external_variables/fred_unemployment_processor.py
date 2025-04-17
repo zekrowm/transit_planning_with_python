a0796fca1570187b1e839947f2157c35ede172ae
zekrowm <153643214+zekrowm@users.noreply.github.com>
2025-04-16 21:21:10 -0400

    style: alphabetize matplotlib imports to follow isort rules

--- scripts/external_variables/fred_unemployment_processor.py
+++ scripts/external_variables/fred_unemployment_processor.py
@@ -12,9 +12,9 @@ graphs to show trends.
 
 import os
 
-import pandas as pd
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # =============================================================================
 # CONFIGURATION
