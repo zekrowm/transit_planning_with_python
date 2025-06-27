006e383db1fc6486066c1f921ec17aeaf3daec57
zekrowm <153643214+zekrowm@users.noreply.github.com>
2025-06-27 19:18:42 -0400

    fix: add missing typing.Mapping import

--- scripts/bus_bay_management/block_status_by_minute_generator.py
+++ scripts/bus_bay_management/block_status_by_minute_generator.py
@@ -9,7 +9,7 @@ though direct execution via the command line is also supported.
 
 import logging
 import os
-from typing import Any, Optional, cast
+from typing import Any, Optional, Mapping, cast
 
 import pandas as pd
 
