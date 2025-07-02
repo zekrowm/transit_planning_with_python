a55d90b97f5f656dd2096ff456625846a49ed55f
zekrowm <153643214+zekrowm@users.noreply.github.com>
2025-07-02 09:24:36 -0400

    chore: add missing typing imports

--- scripts/gtfs_validation/stops_versus_road_name_typo_checker_gpd.py
+++ scripts/gtfs_validation/stops_versus_road_name_typo_checker_gpd.py
@@ -17,7 +17,7 @@ Outputs:
 import logging
 import os
 import re
-from typing import Any, Mapping, Optional
+from typing import Any, Dict, List, Mapping, Optional, Set
 
 import geopandas as gpd
 import pandas as pd
