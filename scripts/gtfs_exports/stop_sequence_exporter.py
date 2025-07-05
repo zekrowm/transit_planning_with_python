a9d181bafcfdba30529c8fdbd76d0b4bb7b1744d
zekrowm <153643214+zekrowm@users.noreply.github.com>
2025-07-05 11:28:01 -0400

    chore: add missing typing.Sequence import

--- scripts/gtfs_exports/stop_sequence_exporter.py
+++ scripts/gtfs_exports/stop_sequence_exporter.py
@@ -15,7 +15,7 @@ import logging
 import os
 import re
 from collections import Counter
-from typing import Any, Mapping, Optional
+from typing import Any, Mapping, Optional, Sequence
 
 import pandas as pd
 
